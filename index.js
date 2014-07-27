eb39cf4d011c59c4e1898761e893eecf2d9610cd
Dylan Visher <dylan.visher@yale.edu>
2014-07-26 18:41:49 -0700

    Fixing up deploy pipeline

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-
 // Posts : [{title: string, entry: domstring}]
-function getPosts() {
+var g_Posts = [];
+var g_NumPosts = 0;
+
+function updatePosts() {
     console.log("getting Posts");
-    $.getJSON("posts/posts.json", function (posts) {
+    var url = "/posts/posts"+g_NumPosts.toString()+".json";
+    $.getJSON(url, function (posts) {
 	console.log("got Posts");
 	console.log(posts);
-	document.querySelector("#posts").posts = posts;
+	$.merge(g_Posts, posts);
+	g_NumPosts += 1;
 	console.log(document.querySelector("#posts"));
     });
 }
 
-var posts;
-
 document.addEventListener('polymer-ready', function () {
-    getPosts();
+    document.querySelector("#posts").posts = g_Posts;
+    updatePosts();
 });
+
+var didScroll = false;
+
+$(window).scroll(function() {
+  didScroll = true;
+});
+
+setInterval(function() {
+    if ( didScroll ) {
+	didScroll = false;
+	if ($(window).scrollTop() >= $(document).height() - $(window).height() - 50) {
+	    updatePosts();
+	}
+    }
+}, 250);
